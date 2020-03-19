e35b68c136aae7b1ecef3aa7dddf8193335736fd
Marius Lazar <mekstyle96@gmail.com>
2020-03-19 15:32:47 +0100

    Nuovo button con animazione carina

--- Coronavirus/Feature/World/WorldSpreadController.swift
+++ Coronavirus/Feature/World/WorldSpreadController.swift
@@ -16,26 +16,39 @@ class WorldSpreadController: UIViewController, CoronavirusDelegate {
     
     var model: WorldSpreadVM!
     var dataManager: DataManager!
+    @IBOutlet weak var refreshButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         model.delegate = self
         model.changeMapStyle()
         model.updateAnnotation(covid: dataManager.coronavirus)
+        configureButton()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         dataManager.delegate = self
     }
-
-    @IBAction func reloadAboutCoronavirus(_ sender: UIBarButtonItem) {
+    
+    @IBAction func reloadAnnotation(_ sender: UIButton) {
         model.showLoading()
         self.dataManager.refreshData()
+        
+        UIButton.animate(withDuration: 1.4, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.refreshButton.transform = self.refreshButton.transform.rotated(by: CGFloat(Double.pi))
+        }, completion: nil)
+        
+        
+
     }
     
     func didUpdateData() {
         model.updateAnnotation(covid: dataManager.coronavirus)
+    }
+    
+    private func configureButton() {
+        refreshButton.layer.cornerRadius = refreshButton.frame.height / 2
     }
 }
 
